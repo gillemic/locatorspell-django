0409a71d0d30df1b0e558afcb4ffcf32ac010cbe
Michael Gillett <michaelgillett19@gmail.com>
2023-06-07 18:10:31 -0700

    Stuff

--- locatorspell/urls.py
+++ locatorspell/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from . import views
+
 urlpatterns = [
-    path("", include("events.urls")),
+    path("", views.index),
     path("events/", include("events.urls")),
     path('admin/', admin.site.urls),
 ]
