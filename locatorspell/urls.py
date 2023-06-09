15df77f35249f1e28de78eba96acd1545736a72d
Michael Gillett <michael.gillett@pacificoffice.com>
2023-06-09 13:27:45 -0700

    About page

--- locatorspell/urls.py
+++ locatorspell/urls.py
@@ -21,6 +21,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index),
+    path("about", views.about, name="about"),
     path("events/", include("events.urls")),
     path('admin/', admin.site.urls),
 ]
--- locatorspell/views.py
+++ locatorspell/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse, Http404
 
 def index(request):
   return render(request, "index.html")
+
+def about(request):
+  return render(request, "about.html")
