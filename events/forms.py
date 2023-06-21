7f53ef33ba5be1cdb490fb8aeabd69fe7321e01a
Michael Gillett <michael.gillett@pacificoffice.com>
2023-06-21 11:02:31 -0700

    Stuff

--- events/forms.py
+++ events/forms.py
@@ -36,7 +36,7 @@ class DateInput(forms.DateInput):
 class EventSearchForm(forms.Form):
     activity = forms.MultipleChoiceField(label='Activity', choices=ACTIVITY_CHOICES, widget=forms.CheckboxSelectMultiple)
     location = forms.ChoiceField(label='Location', choices=LOCATION_CHOICES)
-    start_date = forms.DateField(label='Start Date', widget=DateInput())
-    end_date = forms.DateField(label='End Date', widget=DateInput())
+    start_date = forms.DateField(label='Start Date', widget=DateInput(), initial=date.today())
+    end_date = forms.DateField(label='End Date', widget=DateInput(), required=False)
     price = forms.ChoiceField(label='Price', choices=PRICE_CHOICES)
     category = forms.MultipleChoiceField(label='Category', choices=CATEGORY_CHOICES, widget=forms.CheckboxSelectMultiple)
