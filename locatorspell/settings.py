d758faff77d25484c68f833f57ff9f5adb089d89
Michael Gillett <michael.gillett@pacificoffice.com>
2023-06-22 15:22:49 -0700

    Stuff

--- locatorspell/settings.py
+++ locatorspell/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,12 +132,15 @@ STATICFILES_DIRS = [
 ]
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
     'sass_processor.finders.CssFinder',
 ]
+
+django_heroku.settings(locals())
 
 SASS_PROCESSOR_ROOT = os.path.join(BASE_DIR, 'static')
 
