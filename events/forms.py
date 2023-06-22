6297e090a9cd6cbf4837c8746b977742b3011cae
Michael Gillett <michael.gillett@pacificoffice.com>
2023-06-22 12:34:13 -0700

    Spell Components

--- events/forms.py
+++ events/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from datetime import date
 
 ACTIVITY_CHOICES = (
-    ("Magic", "Magic the Gathering"),
+    ("Magic The Gathering", "Magic The Gathering"),
     ("Pokemon", "Pokemon"),
     ("Yu-Gi-Oh", "Yu-Gi-Oh"),
     ("DnD", "DnD"),
@@ -15,12 +15,12 @@ LOCATION_CHOICES = (
 )
 
 PRICE_CHOICES = (
-    ("0", "Any"),
+    ("Any", "Any"),
     ("0", "Free"),
-    ("5", "$5+"),
-    ("10", "$10+"),
-    ("20", "$20+"),
-    ("50", "$50+"),
+    ("5", "$5"),
+    ("10", "$10"),
+    ("20", "$20"),
+    ("50", "$50"),
 )
 
 CATEGORY_CHOICES = (
@@ -36,7 +36,7 @@ class DateInput(forms.DateInput):
 class EventSearchForm(forms.Form):
     activity = forms.MultipleChoiceField(label='Activity', choices=ACTIVITY_CHOICES, widget=forms.CheckboxSelectMultiple)
     location = forms.ChoiceField(label='Location', choices=LOCATION_CHOICES, required=False)
-    start_date = forms.DateField(label='Start Date', widget=DateInput(), initial=date.today())
-    end_date = forms.DateField(label='End Date', widget=DateInput(), required=False)
+    start_date = forms.DateField(label='After Date', widget=DateInput(), required=False)
+    end_date = forms.DateField(label='Before Date', widget=DateInput(), required=False)
     price = forms.ChoiceField(label='Price', choices=PRICE_CHOICES)
     category = forms.MultipleChoiceField(label='Category', choices=CATEGORY_CHOICES, widget=forms.CheckboxSelectMultiple)
--- events/views.py
+++ events/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
+from dateutil.parser import parse
 
 from .models import Event
 from .forms import EventSearchForm
@@ -10,17 +11,27 @@ def index(request):
   form = EventSearchForm()
   
   if request.method == 'GET':
-    activity = request.GET.get('activity', None)
+    activity = request.GET.getlist('activity')
     location = request.GET.get('location', 'Any')
-    #start_date = request.GET.get('start_date', '')
-    #end_date = request.GET.get('end_date', '')
-    price = request.GET.get('price', 0)
-    category = request.GET.get('category', None)
+    start_date = request.GET.get('start_date', '')
+    end_date = request.GET.get('end_date', '')
+    price = request.GET.get('price', 'Any')
+    category = request.GET.getlist('category')
 
-    if activity is not None:
-      event_list = event_list.exclude(event_type__exact='').filter(event_type=activity)
+    if activity:
+      event_list = event_list.filter(event_type__in=activity)
     if location != 'Any':
-      event_list = event_list.filter(location=location)
+      event_list = event_list.filter(location__icontains=location)
+    if start_date != '':
+      sdate = parse(start_date)
+      event_list = event_list.filter(start_date__gte=sdate)
+    if end_date != '':
+      edate = parse(end_date)
+      event_list = event_list.filter(start_date__lte=edate)
+    if price != 'Any':
+      event_list = event_list.filter(price__lte=price)
+    if category:
+      event_list = event_list.filter(category__in=category)
 
   context = {"event_list": event_list, "form": form}
   return render(request, "events/index.html", context)
