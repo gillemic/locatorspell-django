3ff437284f1328f209f595263388f4af8b3fd78e
Michael Gillett <michael.gillett@pacificoffice.com>
2023-06-22 16:12:34 -0700

    blog on front page

--- locatorspell/views.py
+++ locatorspell/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 
+from blog.models import Blog
+
 def index(request):
-  return render(request, "index.html")
+  blog_posts = Blog.objects.order_by('date_posted')
+  context = { "blog_posts": blog_posts }
+  return render(request, "index.html", context)
 
 def about(request):
   return render(request, "about.html")
